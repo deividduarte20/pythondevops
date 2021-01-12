82ac5d7b925f782211b54472f8974ebafc6ff199
Deivid Duarte <deividdua@gmail.com>
2021-01-12 13:49:46 -0300

    Adicionado arquivo for.py

---
+++ for.py
@@ -0,0 +1,13 @@
+cardapio = ['Carne', 'Queijo', 'Frango', 'Calabresa', 'Pizza', 'Carne Seca']
+
+'''
+print(cardapio[0])
+print(cardapio[1])
+print(cardapio[2])
+'''
+
+print('Pastelaria Devops')
+print('Veja o nosso cardapio')
+print('----------------------')
+for recheio in cardapio:
+    print(f'O recheio do pastel é de {recheio}')
