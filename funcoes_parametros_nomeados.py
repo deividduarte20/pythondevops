865ae645aa90caaa668b3f856eab44072e5e058a
Deivid Duarte <deividdua@gmail.com>
2021-01-18 13:33:12 -0300

    Foram adicionados os arquivos  funcoes_parametros_nomeados.py e funcoes_parametros_opcionais.py

---
+++ funcoes_parametros_nomeados.py
@@ -0,0 +1,4 @@
+def imprima_valores(valor1, valor2):
+    print(f'O valor1 é {valor1}, e o valor2 é {valor2}')
+
+imprima_valores(2,1)
---
+++ funcoes_parametros_opcionais.py
@@ -0,0 +1,4 @@
+def imprima_valores(sabor, status='Disponivel'):
+    print(f'O sabor é {sabor} e o status é {status}')
+
+imprima_valores('Queijo', 'Indisponivel')
