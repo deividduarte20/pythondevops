1fe150c93c44ba32964cda67b222f89869e07cfb
Deivid Duarte <deividdua@gmail.com>
2021-01-28 08:46:52 -0300

    Alterado arquivo calculadora.py

--- projetos/calculando.py
+++ projetos/calculando.py
@@ -1,4 +1,14 @@
-from calculadora import Calculadora
+# from calculadora import Calculadora
 
-objeto = Calculadora(18, 10)
-objeto.soma()
+# objeto = Calculadora(18, 10)
+# objeto.soma()
+
+class Curso:
+
+    def __init__(self, curso, professor):
+        self.curso = curso
+        self.professor = professor
+        
+cursos = Curso('Curso de Python', 'Deivid')
+print(cursos.curso)
+print(cursos.professor)
