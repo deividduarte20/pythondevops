dc705e9b1974fa8de1c5775ebf4bccddd016ca64
Deivid Duarte <deividdua@gmail.com>
2021-02-02 16:53:41 -0300

    Adicionado arquivo funcao

---
+++ function.py
@@ -0,0 +1,26 @@
+# def boas_vindas():
+#     print('Olá Deivid!')
+#     print('Temos 5 laptops em estoque')
+
+# boas_vindas()
+
+# def somar_dois_numeros():
+#     numero1 = 10
+#     numero2 = 5
+#     resultado = numero1 + numero2
+#     print(resultado)
+
+
+# somar_dois_numeros()
+
+# def boas_vindas(nome,quantidade):
+#     print(f'Olá {nome}.')
+#     print(f'Temos {quantidade} laptops em estoque')
+
+# boas_vindas('Maria', 4)
+
+def boas_vindas(nome,quantidade=6):
+    print(f'Olá {nome}.')
+    print(f'Temos {quantidade} laptops em estoque')
+
+boas_vindas('Maria')
