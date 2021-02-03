6f01f28a62625246602c5dd7834ee6db76951cbe
Deivid Duarte <deividdua@gmail.com>
2021-02-03 09:59:12 -0300

    Foi adicionado o arquivo funcao_xargs_varios.py

---
+++ funcao_xargs_varios.py
@@ -0,0 +1,9 @@
+# Criar uma função que armazena numeros e strings (dados)
+
+
+def agencia(**carro):
+    return carro
+
+print(agencia(marca='Gol', cor='Branca', motor=1.0, placa=1234))
+print(agencia(marca='Gol', cor='Preto', motor=1.0))
+print(agencia(marca='Gol', cor='Preto'))
