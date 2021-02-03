74ddd31ee193d94fce318903f4bed13083cf6e62
Deivid Duarte <deividdua@gmail.com>
2021-02-03 10:37:21 -0300

    Foi adicionado desafio1.py

---
+++ desafio1.py
@@ -0,0 +1,26 @@
+'''
+Criar um programa que calcula a quantidade de tinta necessária para pintar
+uma parede. O usuário deverá fornecer as seguintes informações: Rendimento, altura e largura.
+O programa deve mostrar na tela a mensagem 'Você necessita de x latas de tinta'
+Uma lata de tinta pinta 5 metros quadrado
+Calculo:
+Altura * Largura = Metros quadrados / pelo rendimento da lata
+'''
+
+def pintura():
+    lata = int(input('Qual é o rendimento da lata? '))
+    altura = float(input('Qual a altura da parede? '))
+    largura = float(input('Qual a largura da parede? '))
+    resultado = (altura * largura) / lata
+    return f'Você precisa de {resultado} latas de tinta'
+
+print(pintura())
+
+
+
+
+
+
+
+
+
