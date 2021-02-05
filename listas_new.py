35bd25074d27836ff21fa115fcd71be84cee55a5
Deivid Duarte <deividdua@gmail.com>
2021-02-04 21:46:17 -0300

    foi adicionado o arquivo listas_new.py

---
+++ listas_new.py
@@ -0,0 +1,22 @@
+
+# cidade1 = 'Rio de Janeiro'
+# cidade2 = 'São Paulo'
+# cidade3 = 'Salvador'
+
+cidades = ['Rio de Janeiro', 'São Paulo', 'Salvador', 'Goiania','Goiania']
+
+# print(cidades.count('Goiania')) Mostra o número de vezes que a palavra aparece
+# print(cidades)
+
+# cidades.insert(0,'Sergipe') Inserir item em posição especifica da lista
+cidades[0] = 'Pernambuco' 
+print(cidades)
+
+
+# Troca o dado dentro do item
+# cidades[0] = 'Brasilia'
+
+# cidades.append('Santa Catarina')
+# cidades.count()
+# print(cidades)
+
