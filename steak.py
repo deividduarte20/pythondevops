62c44a99010d4c174412a9fcf496d19db4ef1938
Deivid Duarte <deividdua@gmail.com>
2021-02-12 20:28:52 -0300

    foi alterado o arquivo steak.py

--- steak.py
+++ steak.py
@@ -1,9 +1,14 @@
-temperatura = input('Qual a temperatura da carne: ')
+temperatura = int(input('Qual a temperatura da carne: '))
 
-if temperatura <= 48 or temperatura == 120:
-    print('Carne Selada')
-elif temperatura == 54 or temperatura == 130:
+if temperatura < 48: 
+    print('Cozinhar por mais alguns minutos')
+elif temperatura >= 48 and temperatura <= 54:
+    print('Selada') 
+elif temperatura > 54 and temperatura <= 59:
     print('Ao ponto para mal')
-elif temperatura == 60 or temperatura == 140:
+elif temperatura > 59 and temperatura <= 64: 
     print('Ao ponto')
-elif temperatura == 
+elif temperatura > 64 and temperatura <= 71:
+    print('Ao ponto para o bem')
+elif temperatura > 71:
+    print('Bem passada')
