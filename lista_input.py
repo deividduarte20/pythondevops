bf5c682ed2c6ef692d7939da87f3bc106a7ec781
Deivid Duarte <deividdua@gmail.com>
2021-02-13 09:54:39 -0300

    Foi adicionado lista_input.py

---
+++ lista_input.py
@@ -0,0 +1,10 @@
+# Listas
+    # Armazenar mais de uma informação em uma variavel
+    # Manter a sequência dos dados em uma variavel
+
+frutas_usuario = input('Digite o nome das frutas separadas por virgula: ')
+
+frutas_lista = frutas_usuario.split(', ')
+
+print(frutas_lista)
+
