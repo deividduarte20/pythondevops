fe0e20d61fd1c940d51d56a46e358f6ffd525bd0
Deivid Duarte <deividdua@gmail.com>
2021-02-12 21:35:03 -0300

    Foi alterado arquivo steak.py e adicionado loop_lista.py

--- steak.py
+++ steak.py
@@ -1,14 +1,27 @@
 temperatura = int(input('Qual a temperatura da carne: '))
 
-if temperatura < 48: 
+# if temperatura < 48: 
+#     print('Cozinhar por mais alguns minutos')
+# elif temperatura >= 48 and temperatura <= 54:
+#     print('Selada') 
+# elif temperatura > 54 and temperatura <= 59:
+#     print('Ao ponto para mal')
+# elif temperatura > 59 and temperatura <= 64: 
+#     print('Ao ponto')
+# elif temperatura > 64 and temperatura <= 71:
+#     print('Ao ponto para o bem')
+# elif temperatura > 71:
+#     print('Bem passada')
+
+if temperatura < 48:
     print('Cozinhar por mais alguns minutos')
-elif temperatura >= 48 and temperatura <= 54:
-    print('Selada') 
-elif temperatura > 54 and temperatura <= 59:
+elif temperatura in range(48, 53):
+    print('Selada')
+elif temperatura in range(54, 59):
     print('Ao ponto para mal')
-elif temperatura > 59 and temperatura <= 64: 
+elif temperatura in range(60, 64):
     print('Ao ponto')
-elif temperatura > 64 and temperatura <= 71:
+elif temperatura in range(65, 70):
     print('Ao ponto para o bem')
-elif temperatura > 71:
+elif temperatura >= 71:
     print('Bem passada')
