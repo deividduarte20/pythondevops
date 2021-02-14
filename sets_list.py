5d558bfcfdcc195b82d6df81cb605adef809955e
Deivid Duarte <deividdua@gmail.com>
2021-02-14 10:12:09 -0300

    Foram adicionados os arquivos set_listas.py sets.py sets_list.py

---
+++ sets_list.py
@@ -0,0 +1,15 @@
+# Set (Listas)
+    # Similar a listas
+    # Evita itens duplicados
+    # Não utiliza index
+
+set1 = {'a', 'b', 'c'}
+set2 = {'a', 'd', 'e'}
+set3 = {'c', 'd', 'f'}
+
+# set4 = set1.union(set2) # Uni dois conjuntos set1 e set2
+# set4 = set1.difference(set3)
+# set4 = set1.intersection(set2) # Número que está nos doi conjuntos
+set4 = set1.symmetric_difference(set3) 
+
+print(set4)
