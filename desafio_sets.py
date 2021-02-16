e823e6ae9c0a2aa159012170e6f064e32af0884a
Deivid Duarte <deividdua@gmail.com>
2021-02-16 13:26:40 -0300

    Modificação em dois arquivos desafio_sets.py e sets_list.py

--- desafio_sets.py
+++ desafio_sets.py
@@ -9,16 +9,19 @@ Lista3 = Funcionários que não tem carro
 
 '''
 
-funcionarios = {'Ana', 'Marcos', 'Alice', 'Pedro', 'Sophia', 'Bruno', 'Melissa'}
-turno_dia = {'Ana', 'Marcos', 'Alice', 'Melissa'}
-turno_noite = {'Pedro', 'Sophia', 'Bruno'}
-tem_carro = {'Marcos', 'Alice', 'Bruno', 'Melissa'}
+funcionarios = ['Ana', 'Marcos', 'Alice', 'Pedro', 'Sophia', 'Bruno', 'Melissa']
+turno_dia = ['Ana', 'Marcos', 'Alice', 'Melissa']
+turno_noite = ['Pedro', 'Sophia', 'Bruno']
+tem_carro = ['Marcos', 'Alice', 'Bruno', 'Melissa']
 
-lista1 = turno_noite.intersection(tem_carro)
+# Lista1
+lista1 = set(tem_carro).intersection(turno_noite)
 print(lista1)
 
-lista2 = turno_dia.intersection(tem_carro)
+# Lista2
+lista2 = set(tem_carro).intersection(turno_dia)
 print(lista2)
 
-lista3 = funcionarios.difference(tem_carro)
+# Lista3
+lista3 = set(funcionarios).difference(tem_carro)
 print(lista3)
--- sets_list.py
+++ sets_list.py
@@ -9,7 +9,7 @@ set3 = {'c', 'd', 'f'}
 
 # set4 = set1.union(set2) # Uni dois conjuntos set1 e set2
 # set4 = set1.difference(set3)
-# set4 = set1.intersection(set2) # Número que está nos doi conjuntos
+# set4 = set1.intersection(set2) # Número que está nos dos conjuntos
 set4 = set1.symmetric_difference(set3)
 
 print(set4)
