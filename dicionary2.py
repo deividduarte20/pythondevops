8995dbcacc8252ab1919facb4b03f7df0701a734
Deivid Duarte <deividdua@gmail.com>
2021-02-16 12:12:16 -0300

    Adicionando dois novos arquivos dicionários

---
+++ dicionary2.py
@@ -0,0 +1,9 @@
+# Dicionários
+    # Utiliza index no formato de keys e values
+    # Aceita string, integer, float, boolean...
+    # Itera dicionário com keys(Chaves) e values(Valores)
+
+aluno = {'nome': 'Ana', 'idade': 16, 'nota_final': 'A', 'Aprovação': True}
+
+for keys, values in aluno.items():
+    print(keys, values)
