43e58724c26b95ff4944e83130a908c1791d95c7
Deivid Duarte <deividdua@gmail.com>
2021-02-16 12:20:41 -0300

    Adicionado novo arquivo dicionario_more.py

---
+++ dicionario_more.py
@@ -0,0 +1,20 @@
+# Dicionários
+    # Utiliza index no formato de keys e values
+    # Aceita string, integer, float, boolean...
+    # Itera dicionário com keys(Chaves) e values(Valores)
+    # Organizando o dicionário
+
+aluno = {
+    'nome': 'Ana',
+    'idade': 16,
+    'nota_final': 'A',
+    'Aprovação': True,
+    'Materias': ['Fisica', 'Matematica', 'Ingles']
+}
+
+print(aluno.items())
+print(aluno.keys())
+print(aluno.values())
+
+# print(aluno.get('Materias'))
+# print(len(aluno))
