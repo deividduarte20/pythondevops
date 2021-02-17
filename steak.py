07733db3e73d98a872db0dd9cfae54808293412a
unknown <deividdua@gmail.com>
2021-02-17 08:19:51 -0300

    Modificação steak.py

--- steak.py
+++ steak.py
@@ -1,9 +1,14 @@
-temperatura = input('Qual a temperatura da carne: ')
+temperatura = int(input('Qual a temperatura da carne: '))
 
-if temperatura <= 48 or temperatura == 120:
+if temperatura <= 48:
     print('Carne Selada')
-elif temperatura == 54 or temperatura == 130:
+elif temperatura > 48 and temperatura <= 54:
     print('Ao ponto para mal')
-elif temperatura == 60 or temperatura == 140:
+elif temperatura > 54  and temperatura <= 60:
     print('Ao ponto')
-elif temperatura == 
+elif temperatura > 60 and temperatura <= 65:
+    print('Ao ponto para o bem')
+elif temperatura > 65 and temperatura <= 71:
+    print('Bem passada')
+elif temperatura > 71:
+    print('Queimada')
