f169a1ea0a94016d96b3c17670a3a898e6bc6556
Deivid Duarte <48442376+deividduarte20@users.noreply.github.com>
2023-01-22 11:32:08 -0300

    Delete manipulando_sistema_operacional directory

--- manipulando_sistema_operacional/trabalhando_com_info_arquivos.py
+++
@@ -1,7 +0,0 @@
-import os
-
-FILE_NAME = 'variaveis_de_ambiente.py'
-
-# print(os.path.getctime(FILE_NAME))
-# print(os.path.getatime(FILE_NAME))
-
--- manipulando_sistema_operacional/variaveis_de_ambiente.py
+++
@@ -1,6 +0,0 @@
-import os
-
-# print(os.environ['HOME'])
-print(os.environ.get('SHELL2', '/bin/bash'))
-print(os.getenv('HOME'))
-print(os.getenv('MYHOME', '/root'))
