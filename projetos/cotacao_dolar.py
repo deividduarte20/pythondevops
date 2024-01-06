d7f1d90824fd9d17d8c9410f6330702b40f0566b
Deivid Duarte <48442376+deividduarte20@users.noreply.github.com>
2024-01-06 11:17:14 -0300

    Update cotacao_dolar.py

--- projetos/cotacao_dolar.py
+++ projetos/cotacao_dolar.py
@@ -4,4 +4,5 @@ url = requests.get("https://economia.awesomeapi.com.br/last/USD-BRL")
 requisicao_dic = url.json()
 cotacao_dolar = requisicao_dic["USDBRL"]["bid"]
 
-print(cotacao_dolar)
+cotacao_dolar = float(cotacao_dolar)
+print(f"O valor em real é: {cotacao_dolar:.2f}")
